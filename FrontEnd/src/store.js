a6960d28b61ab747f9b3971ea545976c7ce5c25c
Jess37SF <61851109+Jess37SF@users.noreply.github.com>
2020-09-06 19:36:18 +1000

    if case

--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -1,3 +1,29 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
+const initialState = {};
+const middleware = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+    stor = createStore (
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENTION__ &&
+              window.__REDUX_DEVTOOLS_EXTENTION__()
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose (applyMiddleware(...middleware))
+
+        );
+}
+
+export default store;
