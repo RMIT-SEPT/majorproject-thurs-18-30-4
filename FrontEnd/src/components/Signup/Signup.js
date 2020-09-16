2298ba2b1d5c3731469c5bfee61553573858f1b0
s3707659 <s3707659@student.rmit.edu.au>
2020-09-17 09:47:15 +1000

    Styling and adding code for connection between backend and frontend

--- FrontEnd/src/components/Signup/Signup.js
+++ FrontEnd/src/components/Signup/Signup.js
@@ -35,72 +35,82 @@ class SignUp extends Component {
 
   render() {
     return (
-      <div>
-        <div className="form-group">
-          <form onSubmit {...this.onSubmit}>
-            <div className="signup-form">
-              <h1>Sign Up</h1>
-      
-              <label>Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="Name"
-                name="name"
-                value={this.state.name}
-                onChange={this.onChange}
-              ></input>
+      <div className="Signup">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <div className="form-group">
+                <div className="signup-form">
+                  <form onSubmit={this.onSubmit}>
+                    <h1>Sign Up</h1>
 
-              <label>Email address</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                Email="Email"
-                value={this.state.Email}
-                onChange={this.onChange}
-              ></input>
+                   
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="Name"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.onChange}
+                    ></input>
 
-              <label for="role">Account type</label>
-              <select class="form-control" id="role">
-                role = "role"
-                <option>Customer</option>
-                <option>Business Owner</option>
-                value={this.state.role}
-                onChange={this.onChange}
-              </select>
 
-              <label>Username</label>
-              <input
-                type="text"
-                class="form-control"
-                id="username"
-                username="username"
-                value={this.state.username}
-                onChange={this.onChange}
-              ></input>
+                    <label>Email address</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="Email"
+                      Email="Email"
+                      value={this.state.Email}
+                      onChange={this.onChange}
+                    ></input>
+                    
+                    
+             
+                    <label for="role">Account type</label>
+                    <select class="form-control" id="role">
+                      <option>Customer</option>
+                      <option>Business Owner</option>
+                      role ="role"
+                      value={this.state.role}
+                      onChange={this.onChange}
+                    </select>
+         
+                    <label>Username</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="username"
+                      username="username"
+                      value={this.state.username}
+                      onChange={this.onChange}
+                    ></input>
 
-              <label>Create Password</label>
-              <input
-                type="password"
-                class="form-control"
-                password="password"
-                value={this.state.password}
-                onChange={this.onChange}
-              ></input>
+                    <label>Create Password</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      password="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    ></input>
 
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                class="form-control"
-                password="password"
-                value={this.state.password}
-                onChange={this.onChange}
-              ></input>
-              <button variant="primary">Sign Up </button>
-              {""}
+                    <label>Confirm Password</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      password="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    ></input>
+                    <button variant="primary">Sign Up </button>
+                    {""}
+                  </form>
+                </div>
               </div>
-          </form>
+            </div>
+          </div>
         </div>
       </div>
     );
