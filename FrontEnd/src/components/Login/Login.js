b95433bf0bbfa6bff8e52e0f242024cd7effd9c6
Jacob <govern01mail@gmail.com>
2020-09-18 14:54:30 +1000

    Signup and Login connect to backend ports

--- FrontEnd/src/components/Login/Login.js
+++ FrontEnd/src/components/Login/Login.js
@@ -1,31 +1,77 @@
 import React, { Component } from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../Layout/Style/style.css";
+import axios from "axios";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state={
+      "username": "",
+      "password": ""
+    }
+    this.onChange= this.onChange.bind(this);
+    this.onSubmit= this.onSubmit.bind(this);
+  }
+  onChange(e){
+    this.setState({[e.target.id]: e.target.value});
+  }
+  onSubmit(e){
+    e.preventDefault();
+    // REST request
+    axios({
+      method: "post",
+      url: "http://localhost:8080/api/auth/signin",
+      data: {
+        username: this.state.username,
+        password: this.state.password
+      }
+    }).then(function(response) {
+      console.log(response.status);
+      // TODO
+      //   save auth token
+      //   alert user login successful
+      //   navigate to homepage
+    }).catch(function(error){
+      // handle error
+      if (error.response){
+        console.log(error.response.status);
+        console.log(error.response.data);
+        // TODO expand error handling: handle codes 400 (empty values), 401 (login failed), 500 (backend error)
+      } else if (error.request){
+        console.log("Request made, no response");
+        console.log(error.request);
+      } else{
+        console.log("Error occurred: ", error.message);
+      }
+    });
+  }
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group ">
             <div className="login-form">
               <h1>Log In</h1>
 
-              <label>Email address</label>
+              <label>Username</label>
               <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                placeholder="Enter email"
+                type="username"
+                className="form-control"
+                id="username"
+                onChange={this.onChange}
+                value={this.state.username}
               ></input>
 
               <label>Password</label>
               <input
                 type="password"
-                class="form-control"
-                placeholder="Enter password"
+                className="form-control"
+                id="password"
+                onChange={this.onChange}
+                value={this.state.password}
               ></input>
-              <button variant="primary">Log In </button>
+              <button variant="primary" disabled={ !(this.state.password !== "" && this.state.username !== "") }>Log In </button>
               {""}
             </div>
           </div>
